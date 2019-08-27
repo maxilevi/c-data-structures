3c2a9e5f7e7dfaf9341b2ce61fa49b19ebef19d1
Zaphyk <zaphyk@live.com>
2019-08-27 09:54:55 -0300

    added the rest of functions

--- tp0/tp0.c
+++ tp0/tp0.c
@@ -6,21 +6,40 @@
  * *****************************************************************/
 
 void swap (int *x, int *y) {
-	int temp = *x;
-	*x = *y;
-	*y = temp;
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 
 int maximo(int vector[], int n) {
-    return -1;
+    if(n == 0) return -1; 
+    int posicion = 0;
+    for(int i = 0; i < n; ++i) {
+        if(vector[i] > vector[posicion]) {
+            posicion = i;
+        }
+    }
+    return posicion;
 }
 
 
 int comparar(int vector1[], int n1, int vector2[], int n2) {
+    if(n1 != n2)
+        return n1 < n2 ? -1 : 1;
+
+    for(int i = 0; i < n1; ++i) {
+        int num1 = vector1[i];
+        int num2 = vector2[i];
+        if(num1 != num2)
+            return num1 < num2 ? -1 : 1;
+    }
     return 0;
 }
 
 
 void seleccion(int vector[], int n) {
+    for(int i = n-1; i > -1; --i) {
+        swap(i, maximo(vector, i));
+    }
 }
