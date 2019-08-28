fe89c399d6db3f7d546233496949d9b46436034c
Zaphyk <zaphyk@live.com>
2019-08-28 13:10:21 -0300

    passes all tests

--- tp0/tp0.c
+++ tp0/tp0.c
@@ -25,21 +25,24 @@ int maximo(int vector[], int n) {
 
 
 int comparar(int vector1[], int n1, int vector2[], int n2) {
-    if(n1 != n2)
-        return n1 < n2 ? -1 : 1;
-
-    for(int i = 0; i < n1; ++i) {
+    int minimo = n1 < n2 ? n1 : n2;
+    for(int i = 0; i < minimo; ++i) {
         int num1 = vector1[i];
         int num2 = vector2[i];
         if(num1 != num2)
             return num1 < num2 ? -1 : 1;
     }
-    return 0;
+    if(n1 == n2)
+        return 0;
+    else if(n1 < n2)
+        return -1;
+    else
+        return 1;
 }
 
 
 void seleccion(int vector[], int n) {
     for(int i = n-1; i > -1; --i) {
-        swap(i, maximo(vector, i));
+        swap(&vector[i], &vector[maximo(vector, i+1)]);
     }
 }
